92584222558fd01217cb4ae7e761541fef0b336e
pguru1298 <pguru@purdue.edu>
2016-12-01 20:34:10 -0500

    Now adding html additions

--- server.js
+++ server.js
@@ -12,18 +12,15 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+
+
+app.get('/tvshows',function (req, res) {
+
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-
-app.get('/movies',function (req, res) {
-    getMovie("Storks").then(function(value){
-            res.send("Movie: " + value.results[0].original_title + " Summary: " + value.results[0].overview + " ID: " + value.results[0].id);   
-    }, function(err){
-        console.log(err);
-    })
-});
-
 
 function getMovie (movie){
   var options = { 
@@ -56,6 +53,30 @@ request(options, function (error, response, info) {
   // returns after digging through the numbers and returns the idea of result
 });}
 
+
+function getShow(program){
+var options = { 
+  method: 'GET',
+    url: 'https://api.themoviedb.org/3/search/tv',
+    qs: { page: '1',
+        query: program,
+        language: 'en-US',
+        api_key: '2f4fe68403709710f406f4da7be00736' 
+        },
+    body: '{}' 
+  };
+
+return rP(options);
+}
+
+app.get('/movies222',function (req, res) {
+    getMovie("Storks").then(function(value){
+        //console.log(value.results[0]);
+            res.send("Movie: " + value.results[0].original_title + " Summary: " + value.results[0].overview + " ID: " + value.results[0].id);   
+    }, function(err){
+        console.log(err);
+    })
+});
 
 // function getGenre (back)
 // {var options = { method: 'GET',
